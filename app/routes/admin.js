34f134a0f049f2060255773bed0c825a107c7bbb
Joe Kramer <joekramer1125@gmail.com>
2017-08-08 16:26:58 -0700

    error with saving data from admin. functionality everywhere else correct

--- app/routes/admin.js
+++ app/routes/admin.js
@@ -1,4 +1,17 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+  actions: {
+    saveNewBook() {
+      var params = {
+        author: this.get('owner'),
+        title: this.get('title'),
+        summary: this.get('summary'),
+        image: this.get('image') ? this.get('image') : ""
+      };
+      var newBook = this.store.createRecord('book', params);
+      newBook.save();
+      this.transitionTo('index');
+    }
+  }
 });
